d4ba62ee780a523192eb471a920052b853c88e08
MasterOfTheTiger <MasterOfTheTiger>
2018-02-03 15:38:12 -0800

    Made APOD work

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-REQURL = "https://astronomy-pic-of-the-day.herokuapp.com/api.json"
+/*REQURL = "https://astronomy-pic-of-the-day.herokuapp.com/api.json"
 resObject = {}
 function setImage(res){
   var resultURL = res.url
@@ -28,4 +28,34 @@ document.addEventListener('DOMContentLoaded', function () {
   $("#image").click(function(){
     $("#explanation").toggle()
   })
+});*/
+
+
+
+var url = "https://api.nasa.gov/planetary/apod?api_key=f88nlByrAKllCaklW1AtfDuqiAUKAinSni0EcjhW";
+
+
+$.ajax({
+  url: url,
+  success: function(result){
+  if("copyright" in result) {
+    $("#copyright").text("Image Credits: " + result.copyright);
+  }
+  else {
+    $("#copyright").text("Image Credits: " + "Public Domain");
+  }
+
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "none");
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "none");
+    $("#apod_img_id").attr("src", result.url);
+  }
+  $("#reqObject").text(url);
+  $("#returnObject").text(JSON.stringify(result, null, 4));
+  $("#apod_explaination").text(result.explanation);
+  $("#apod_title").text(result.title);
+}
 });
